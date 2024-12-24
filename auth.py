a781d7a1a415de73ccf89214f37e2743e63f1e75
sheffykhlg <56907336+sheffykhlg@users.noreply.github.com>
2024-12-24 21:12:44 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -2,6 +2,6 @@ import os
 
 
 class Vauth:
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
-    API_ID = int(os.getenv("API_ID"))
-    API_HASH = os.getenv("API_HASH")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "7966226342:AAEgJwv_4ej7VN-8KwFsAYlSbiSYwhQ0KDw")
+    API_ID = int(os.getenv("API_ID", "20589413"))
+    API_HASH = os.getenv("API_HASH", "654d559a9a91daeecd9e760fc73e6766")
